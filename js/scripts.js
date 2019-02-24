2939c852f4d3508bd69c0d438118ae10bff4169d
Lex Wilkins <alexwilkins99@yahoo.com>
2019-02-24 12:49:46 -0800

    this will create branching statements that retreive the final vacation value and store it in the results div

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,48 @@
 $(document).ready(function(){
-  debugger;
   $('#resultsdiv').hide();
 
   $('form#vacation-form').submit(function(){
     var weather = $('input:radio[name=weather]:checked').val();
-    var people = $('input:radio[name=people]:checked').val();
-    var geo = $('input:radio[name=geo]:checked').val();
     var travel = $('input:radio[name=travel]:checked').val();
+    var geo = $('input:radio[name=geo]:checked').val();
+
     var userName = $('input#username').val();
 
     if (weather === 'hot') {
-
+      if (travel === 'yes') {
+        if (geo === 'city') {
+          var finalLoc = 'Sao Paolo, Brazil';
+        } else if (geo === 'town') {
+          var finalLoc = 'Tijuana, Mexico';
+        }
+      } else if (travel === 'no') {
+        if (geo === 'city') {
+          var finalLoc = 'Los Angeles, California';
+        } else if (geo === 'town') {
+          var finalLoc = 'Reno, Nevada';
+        }
+    } else if (weather === 'cold') {
+      if (travel === 'yes') {
+        if (geo === 'city') {
+          var finalLoc = 'St. Petersburg, Russia';
+        } else if (geo === 'town') {
+          var finalLoc = 'Galway, Ireland';
+        }
+      } else if (travel === 'no') {
+        if (geo === 'city') {
+          var finalLoc = 'Minneapolis, Minnesota';
+        } else if (geo === 'town') {
+          var finalLoc = 'Boulder, Colorado';
+          }
+        }
+      }
     }
-
-    $('#vaca').append(userName + weather + ' ' + people + ' ' + geo + ' ' + travel);
+    $('#vaca').append(finalLoc);
     $('#resultsdiv').show();
 
 
     event.preventDefault();
   });
+
+
 });
