9b0f141d520c64eac681d538372a7d7157ed11c4
Lex Wilkins <alexwilkins99@yahoo.com>
2019-02-23 17:52:12 -0800

    create submit button with initial js that collects user input, still non functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+  $('#resultsdiv').hide();
 
-  var weather = $('input:radio[name=weather]')
+  $('#vacation-form form').submit(function(){
+    var weather = $('input:radio[name=weather]')
+    var people = $('input:radio[name=people]')
+    var geo = $('input:radio[name=geo]')
+    var travel = $('input:radio[name=travel]')
+
+    $('#vaca') = (weather + ' ' + people + ' ' + geo + ' ' +. travel).text()''
+    $('#resultsdiv').show();
+
+
+    event.preventDefault();
+  });
+
+  event.preventDefault();
 });
