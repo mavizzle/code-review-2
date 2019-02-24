11c50db4621aade02f6fc96ac8d34e696962ce06
Lex Wilkins <alexwilkins99@yahoo.com>
2019-02-24 12:05:46 -0800

    this will allow jquery to capture user input and it now being stored in vars, also corrects syntax errors that prevented jquery from functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
+  debugger;
   $('#resultsdiv').hide();
 
-  $('#vacation-form form').submit(function(){
-    var weather = $('input:radio[name=weather]')
-    var people = $('input:radio[name=people]')
-    var geo = $('input:radio[name=geo]')
-    var travel = $('input:radio[name=travel]')
+  $('form#vacation-form').submit(function(){
+    var weather = $('input:radio[name=weather]:checked').val();
+    var people = $('input:radio[name=people]:checked').val();
+    var geo = $('input:radio[name=geo]:checked').val();
+    var travel = $('input:radio[name=travel]:checked').val();
+    var userName = $('input#username').val();
 
-    $('#vaca') = (weather + ' ' + people + ' ' + geo + ' ' +. travel).text()''
+    if (weather === 'hot') {
+
+    }
+
+    $('#vaca').append(userName + weather + ' ' + people + ' ' + geo + ' ' + travel);
     $('#resultsdiv').show();
 
 
     event.preventDefault();
   });
-
-  event.preventDefault();
 });
